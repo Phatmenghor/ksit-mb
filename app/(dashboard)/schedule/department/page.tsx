b2383c07ee71a18fc3a32c5cd4f3d9403f04119f
phoeungleanghourng <phoeungleanghourng85@gmail.com>
2025-05-18 22:17:12 +0700

    schedule_department_list

---
+++ app/(dashboard)/schedule/department/page.tsx
@@ -0,0 +1,130 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import { Search, ChevronRight } from "lucide-react";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { DepartmentIcon } from "@/components/dashboard/schedule/department/department-icon";
+import DepartmentCard from "@/components/dashboard/schedule/department/department-card";
+import { AllDepartmentModel } from "@/model/master-data/department/all-department-model";
+import { useCallback, useEffect, useState } from "react";
+import { getAllDepartmentService } from "@/service/master-data/department.service";
+import { toast } from "sonner";
+import { ROUTE } from "@/constants/routes";
+import { AllDepartmentFilterModel } from "@/model/master-data/department/type-department-model";
+import { Constants } from "@/constants/text-string";
+import PaginationPage from "@/components/shared/pagination-page";
+
+export default function DepartmentListPage() {
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [allDepartmentData, setAllDepartmentData] =
+    useState<AllDepartmentModel | null>(null);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+
+  const loadDepartments = useCallback(
+    async (param: AllDepartmentFilterModel) => {
+      setIsLoading(true);
+
+      try {
+        const response = await getAllDepartmentService({
+          search: searchQuery,
+          status: Constants.ACTIVE,
+          ...param,
+        });
+
+        if (response) {
+          setAllDepartmentData(response);
+        } else {
+          console.error("Failed to fetch departments:");
+        }
+      } catch (error) {
+        toast.error("An error occurred while loading departments");
+      } finally {
+        setIsLoading(false);
+      }
+    },
+    [searchQuery]
+  );
+
+  useEffect(() => {
+    loadDepartments({});
+  }, [loadDepartments]);
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(e.target.value);
+  };
+  return (
+    <div className="container mx-auto py-6 space-y-6">
+      <Card>
+        <CardContent className="p-6 space-y-2">
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbLink href={ROUTE.DASHBOARD}>Home</BreadcrumbLink>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbPage>Departmemt List</BreadcrumbPage>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+          <h3 className="text-xl font-bold">Manage Schedule</h3>
+          <div className="mb-4 flex flex-col md:flex-row md:items-center md:justify-between gap-4">
+            <div className="relative w-full md:w-1/2">
+              <Search className="absolute left-2.5 top-1/2 -translate-y-1/2 h-4 w-4 text-muted-foreground" />
+              <Input
+                type="search"
+                placeholder="Search department..."
+                className="pl-8 w-full"
+                value={searchQuery}
+                onChange={handleSearchChange}
+              />
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+
+      <div className="bg-white rounded-lg p-6 shadow-sm border">
+        {/* <div className="mb-6">
+          <p className="text-muted-foreground">
+            Total Department: {allDepartmentData?.content.length}
+          </p>
+        </div> */}
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+          {allDepartmentData?.content?.length === 0 ? (
+            <p>No Department found</p>
+          ) : (
+            allDepartmentData?.content?.map((department) => (
+              <DepartmentCard
+                key={department.id}
+                name={department.name}
+                imageUrl={department.urlLogo}
+                imageName={department.name}
+              />
+            ))
+          )}
+        </div>
+      </div>
+      {!isLoading && allDepartmentData && (
+        <div className="mt-4 flex justify-end">
+          <PaginationPage
+            currentPage={allDepartmentData.pageNo}
+            totalPages={allDepartmentData.totalPages}
+            onPageChange={(page: number) => loadDepartments({ pageNo: page })}
+          />
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ components/dashboard/schedule/department/department-card.tsx
@@ -0,0 +1,39 @@
+import { Card } from "@/components/ui/card";
+import { DepartmentIcon } from "./department-icon";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { ChevronRight } from "lucide-react";
+
+function DepartmentCard({
+  name,
+  imageUrl,
+  imageName,
+}: {
+  name: string;
+  imageUrl: string;
+  imageName: string;
+}) {
+  return (
+    <Card className="flex items-center justify-between p-4 hover:bg-muted/20 transition-colors">
+      <div className="flex items-center gap-4">
+        <DepartmentIcon imageUrl={imageUrl} imageName={imageName} />
+        <div>
+          <p className="text-sm text-muted-foreground">Dep.</p>
+          <p className="font-medium">{name}</p>
+        </div>
+      </div>
+      <Button
+        variant="ghost"
+        size="sm"
+        className="text-green-600 hover:text-green-700 hover:bg-green-50"
+        asChild
+      >
+        <Link href="#">
+          View Class
+          <ChevronRight className="ml-1 h-4 w-4" />
+        </Link>
+      </Button>
+    </Card>
+  );
+}
+export default DepartmentCard;
