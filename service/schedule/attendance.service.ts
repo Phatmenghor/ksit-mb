cfe67e75f3f08e9d27b9f22848f49f4b52e81a8a
PhatMenghor <phatmenghor19@gmail.com>
2025-06-20 09:35:08 +0700

    add

--- service/schedule/attendance.service.ts
+++ service/schedule/attendance.service.ts
@@ -18,6 +18,21 @@ export async function getAllAttendanceGenerateService(
   }
 }
 
+export async function getAllAttedanceHistoryService(
+  data: AttendanceHistoryFilter
+) {
+  try {
+    const response = await axiosClientWithAuth.post(
+      `/v1/attendance/history`,
+      data
+    );
+    return response.data.data;
+  } catch (error: any) {
+    console.error("Error fetching requests:", error);
+    return null;
+  }
+}
+
 // update attendance session
 export async function updateAttendanceSessionService(
   data: UpdateAttendanceModel
@@ -30,21 +45,6 @@ export async function updateAttendanceSessionService(
     return response.data.data;
   } catch (error: any) {
     console.error("Error updating attendance session:", error);
-    return null;
-  }
-}
-
-export async function getAllAttendanceHistoryService(
-  data: AttendanceHistoryFilter
-) {
-  try {
-    const response = await axiosClientWithAuth.post(
-      `/v1/attendance/history`,
-      data
-    );
-    return response.data.data;
-  } catch (error: any) {
-    console.error("Error fetching requests:", error);
     return null;
   }
 }
