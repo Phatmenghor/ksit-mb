b1e01c51cc6471633cbc9fa31f00f813c638542d
Nob Makara <makarakara79@gmail.com>
2025-07-15 20:29:47 +0700

    Fix profile

--- components/dashboard/users/shared/user-personal-history.tsx
+++ components/dashboard/users/shared/user-personal-history.tsx
@@ -1,16 +1,20 @@
 import { Fragment } from "react";
 
-// components/shared/InfoGrid.tsx
 interface InfoGridProps {
   data: { label: string; value: string | number | undefined | null }[];
   columns?: number; // Optional: default to 2 columns
 }
 
 export default function InfoGrid({ data, columns = 2 }: InfoGridProps) {
+  const gridColsClass =
+    columns === 1
+      ? "grid-cols-2"
+      : columns === 2
+      ? "grid-cols-4"
+      : "grid-cols-2"; // fallback
+
   return (
-    <div
-      className={`grid grid-cols-${columns * 2} gap-x-4 gap-y-3 text-sm mt-4`}
-    >
+    <div className={`grid ${gridColsClass} gap-x-4 gap-y-3 text-sm mt-4`}>
       {data.map((item, index) => (
         <Fragment key={index}>
           <div className="text-muted-foreground">{item.label}</div>
